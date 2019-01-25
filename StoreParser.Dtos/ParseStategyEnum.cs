83a9736d370c82dea3a5c1e0aa21e37218131348
Максим Акименко <Akymenko.M.O@nmu.one>
2019-01-26 00:24:36 +0200

    Add parsing configuration classes

---
+++ StoreParser.Dtos/ParseStategyEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreParser.Dtos
+{
+    public enum ParseStategyEnum : int
+    {
+        CitrusParserStategy = 1,
+        Other = 2
+    }
+}
---
+++ StoreParser.Dtos/ParsingConfiguration .cs
@@ -0,0 +1,9 @@
+﻿namespace StoreParser.Dtos
+{
+    public class ParsingConfiguration
+    {
+        public string Path { get; set; }
+        public int AmountItems { get; set; }
+        public ParseStategyEnum Stategy { get; set; }
+    }
+}
