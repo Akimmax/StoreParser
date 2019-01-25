8b577ce7a595ddde47c29fddb0f35aee76fc16d2
Максим Акименко <Akymenko.M.O@nmu.one>
2019-01-26 00:12:53 +0200

    Add basic entities

---
+++ StoreParser.Data/Entities/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreParser.Data
+{
+    public class Item
+    {
+        public Item()
+        {
+            Prices = new List<Price>();
+        }
+
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string ImageSource { get; set; }
+        public string ProductUrl { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Price> Prices { get; set; }
+
+    }   
+}
---
+++ StoreParser.Data/Entities/Price.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StoreParser.Data
+{
+    public class Price
+    {
+        public int Id { get; set; }
+
+        public int ItemId { get; set; }
+        public virtual Item Item { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+        public double CurrentPrice { get; set; }
+    }
+}
